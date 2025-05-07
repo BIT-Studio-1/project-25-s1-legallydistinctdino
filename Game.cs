f489c4490f20e4d8eaf7a9277abd98bcf355186d
Samuel Davies (1000048175) <DAVISS6@student.op.ac.nz>
2025-05-08 11:48:58 +1200

    Added Inital Methods

---
+++ Game.cs
@@ -0,0 +1,46 @@
+﻿namespace LegallyDistinctDino
+{
+    internal class Game
+    {
+        // Starting point for code, anyone can adjust as needed
+        static void Main(string[] args)
+        {
+
+        }
+
+        // Alina
+        static void TitleScreen()
+        {
+
+        }
+
+        // Maria
+        static void MainMenu()
+        {
+
+        }
+
+        // Samuel
+        static void SetupGame()
+        {
+
+        }
+
+        // Braedon
+        static void CreateCharacter()
+        {
+
+        }
+
+        // Samuel & Braedon
+        static void PlayGame()
+        {
+
+        }
+
+        static void EndGame()
+        {
+
+        }
+    }
+}
