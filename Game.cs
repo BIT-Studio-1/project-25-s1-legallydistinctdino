fdeffdc29b228d8bbe762741775791e530237fa8
unknown <potam2@student.op.ac.nz>
2025-05-12 08:47:21 +1200

    main menu update

--- Game.cs
+++ Game.cs
@@ -17,6 +17,35 @@
         // Maria
         static void MainMenu()
         {
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("Press 0 to start");
+            Console.WriteLine("Press 1 : Create Character");
+            Console.WriteLine("Press 2 : Settings");
+            Console.WriteLine("Press 3 : Exit");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 0:
+                    PlayGame();
+                    break;
+
+                case 1:
+                    CreateCharacter();
+                    break;
+
+                case 2:
+                    SetupGame();
+                    break;
+
+                case 3:
+                    EndGame();
+                    break;
+
+
+
+
+            }
 
         }
 
